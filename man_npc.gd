5c8c0b859d216106c70a59ce25131445bb129461
Eepr Drmr <astonisherred@gmail.com>
2026-01-31 10:58:59 +0200

    Moving NPCs

--- man_npc.gd
+++ man_npc.gd
@@ -1,9 +1,23 @@
 class_name NPC extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
+var destination: Vector2
+var dir: Vector2
+@export var walk_time: float = 1
+@export var search_radius: float = 5
+@onready var cooldown_timer: Timer = %"Cooldown Timer"
+
 func _ready() -> void:
-	#position += Vector2(randf_range(-1.5, 1.5), randf_range(-1.5, 1.5))
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var rand_offset: Vector2 = Vector2(randf_range(-5, 5), randf_range(-2, 5.5))
+	position += rand_offset
+	cooldown_timer.timeout.connect(walk_around)
+	walk_around()
+
+func walk_around() -> void:
+	destination = global_position + Vector2(randf_range(-search_radius, search_radius), randf_range(-search_radius, search_radius))
+	var t: Tween = create_tween()
+	t.tween_property(self, "global_position", destination, walk_time)
+	start_cooldown()
+
+func start_cooldown() -> void:
+	cooldown_timer.wait_time = randf_range(1, 4)
+	cooldown_timer.start()
