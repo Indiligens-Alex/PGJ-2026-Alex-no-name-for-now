6fdf2bd5c02e4a7b0925e056d124684ef14c7f73
Indiligens <152096497+Indiligens-Alex@users.noreply.github.com>
2026-01-31 19:54:23 +0200

    Ambience Changing on Belonging

--- ambiance_player.gd
+++ ambiance_player.gd
@@ -1,14 +1,29 @@
 extends AudioStreamPlayer
 
-const MUTE_VOL := -50
-@export var player: Player
-@export var transition: float
+const MUTE_VOL := -50.0
+const MAX_VOL := -5.0
 
+@export var fade_speed: float = 5
 
-#func _ready() -> void:
-	#pass 
+func _ready() -> void:
+	stream.set_sync_stream_volume(0, MAX_VOL)
+	stream.set_sync_stream_volume(1, MUTE_VOL)
 
-func _process(_delta: float) -> void:	
+func _process(delta: float) -> void:
 	if main.belonging < 50:
-		pass
-	#print(stream.stream_0.stream)
+		stream.set_sync_stream_volume(0, move_toward(
+			stream.get_sync_stream_volume(0), MAX_VOL,
+				fade_speed * delta))
+		stream.set_sync_stream_volume(1, move_toward(
+			stream.get_sync_stream_volume(1), MUTE_VOL,
+				fade_speed*2 * delta))
+	else:
+		stream.set_sync_stream_volume(0, move_toward(
+			stream.get_sync_stream_volume(0), MUTE_VOL,
+				fade_speed * delta))
+		stream.set_sync_stream_volume(1, move_toward(
+			stream.get_sync_stream_volume(1), MAX_VOL,
+				fade_speed*2 * delta))
+	#
+	#print(stream.get_sync_stream_volume(0),
+	#stream.get_sync_stream_volume(1))
--- test_world_0.gd
+++ test_world_0.gd
@@ -1,18 +1,13 @@
 extends Node
 
 enum GameState { Game, Win, Lose }
-@onready var button: Button = $Button
-
 var state: GameState = GameState.Game
+
 @onready var player: Player = $Player
 @onready var npc_collection: Node = %"NPC Collection"
 
 func _ready() -> void:
-	button.hide()
 	npc_collection.won.connect(game_win)
 
 func game_win():
-	button.show()
-
-func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	pass
